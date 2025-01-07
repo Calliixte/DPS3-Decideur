5dcf0153de073daabd1f218644e4ab4f34459106
Calliixte <alicee.baco@gmail.com>
2025-01-07 15:15:19 +0100

    ajout login user

--- FetchJSON.java
+++ FetchJSON.java
@@ -13,10 +13,49 @@ import org.json.JSONObject; // Assure-toi d'avoir la bibliothèque JSON dans ton
 
 public class FetchJSON {
 	
-	public static int loginDecideur(String identifiant,String pass) {
+	public static int loginDecideur(String identifiant,String pass) throws IOException{
+		/*
+		 * Arguments : 
+		 * 		- idVote -> id du vote dont on veut les informations 
+		 * 		- idVotant -> id de la personne dont on veut le vote, ne sevira pas ici mais est necessaire à l'api
+		 * 		- idGroupe -> id du groupe dans lequel le vote est crée
+		 * Sortie : 
+		 * 		- un objet JSON qui contient toutes les informations liées au vote
+		 * Contenu de la fonction : 
+		 * 		- la fonction établit une connexion avec le serveur,envoie la requete Get, vérifie que le code de retour est le bon, puis convertit le contenu
+		 * 			obtenu en JSON et renvoie cet objet
+		 */
+		JSONObject jsonVoulu = null;
+		int reponse =-1;
+        String baseUrl = "https://projets.iut-orsay.fr/saes3-vjacqu3/classePHP/rest/APILogin.php?login_utilisateur="+identifiant+"&password_utilisateur=";
+        String urlString = baseUrl + pass; 
+        URL url = new URL(urlString);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        //Vérifier le code de réponse HTTP pour du debug
+        int responseCode = connection.getResponseCode();
+        System.out.println("Response Code for request " + responseCode); 
+        
+        //lire la réponse en s'assurant que le code est le bon
+        if (responseCode == HttpURLConnection.HTTP_OK) {  // on verifie que le code renvoie un ok
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            // Convertir la réponse en JSON
+            String strReponse = response.toString();
+            System.out.println(strReponse);
+            reponse = Integer.parseInt(strReponse);
+        }else {
+        	throw new IOException("Login ou mot de passe incorrect");
+        }
+        connection.disconnect();
+		return reponse;
 		
-		
-		return 0;
 	}
 	
 	public static JSONObject recupJSONVote(int idVote,int idVotant,int idGroupe) throws IOException {
@@ -104,6 +143,19 @@ public class FetchJSON {
 		return jsonVoulu;
 		
 	}
- 
+	public static void main(String[] args) {
+		try {
+			int idUser = loginDecideur("remib", "remi");
+			JSONObject j = recupJSONUtilisateur(idUser);
+			System.out.println("idutilisateur :" + j.getInt("idUtilisateur")); 
+			System.out.println("pseudo : " + j.getString("pseudo"));
+			System.out.println("nom : "+j.getString("nom"));
+			System.out.println("prenom : "+j.getString("prenom"));
+			System.out.println("lienpfp : "+j.getString("lienPhotoProfil"));
+		}catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
 }
 
--- GestionGroupes.java
+++ GestionGroupes.java
@@ -313,51 +313,5 @@ public class GestionGroupes {
             }
 		}
 		
-		/*
-		ArrayList<Vote> a = new ArrayList<>();
-		a = MaxNbVotesSatisfaits.gloutonSeuil(g,80);
-		//a = MaxNbVotesSatisfaits.bruteForce(g);
-		ArrayList<String> t = new ArrayList<>();
-		a= Diversite.bruteForceExclusif(g,t);
-		int totalSatis = 0;
-		int totalVotes = 0;
-		for(int i =0;i<a.size();i++) {
-			a.get(i).afficherVote();
-			totalSatis+= a.get(i).nbVotantsGagnant();
-			totalVotes+=a.get(i).getTotalVotant();
-		}
-		System.out.println("total Votes satisfaits : " + totalSatis);
-		System.out.println("total Votants votes choisis : " + totalVotes);
-		
-		System.out.println("-----------------------------------");
-		
-		a = MinBudget.satisfactionMoyenneBruteForce(g.getListeVote(),60);
-		
-		System.out.println("brute force");
-		System.out.println(MinBudget.getSatisfactionMoyenne(a));
-		System.out.println(MinBudget.getSumBudget(a));
-		
-		a = MinBudget.satisfactionMoyenneGlouton(g,60);
-		
-		System.out.println("glouton");
-		System.out.println(MinBudget.getSatisfactionMoyenne(a));
-		System.out.println(MinBudget.getSumBudget(a));
-		
-		a = MinBudget.minimiserBudgetBruteForce(g.getListeVote(), 15000);
-		
-		System.out.println("minimiser brute force");
-		System.out.println(MinBudget.getSatisfactionMoyenne(a));
-		System.out.println(MinBudget.getSumBudget(a));
-		
-		a = MinBudget.minimiserBudgetGlouton(g, 15000);
-		
-		System.out.println("minimiser glouton");
-		System.out.println(MinBudget.getSatisfactionMoyenne(a));
-		System.out.println(MinBudget.getSumBudget(a));
-		
-		for(int i =0;i<a.size();i++) {
-			a.get(i).afficherVote();
-		}
-		*/
 	}
 }
